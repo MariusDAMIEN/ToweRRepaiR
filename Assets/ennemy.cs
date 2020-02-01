9f12af9463c29b5d6781d94ef05933b7ff36f957
Noé Campo <58589179+noe-gif@users.noreply.github.com>
2020-02-01 17:53:12 +0100

    Add files via upload

---
+++ Assets/ennemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ennemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];   
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+    }
+}
